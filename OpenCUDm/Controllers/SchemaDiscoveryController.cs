5f05984aab8c478ee50afd6b325e19739d71d84b
Lonnie Watson <harlock123@gmail.com>
2021-02-15 14:21:27 -0500

    Implemented some new endpoints in SchemaDiscovery

--- OpenCUDm/Controllers/SchemaDiscoveryController.cs
+++ OpenCUDm/Controllers/SchemaDiscoveryController.cs
@@ -21,7 +21,8 @@ namespace OpenCUDm.Controllers
     public class SchemaDiscoveryController : ControllerBase
     {
         [HttpGet]
-        public string Get(string CN)
+        [Route("GetAll")]
+        public string GetAll(string CN)
         {
             string result = "RESULT";
 
@@ -36,6 +37,73 @@ namespace OpenCUDm.Controllers
                             "from INFORMATION_SCHEMA.COLUMNS as A " +
                             "LEFT OUTER JOIN " +
                             "INFORMATION_SCHEMA.TABLES B on A.TABLE_NAME = B.TABLE_NAME " +
+                            "ORDER BY A.TABLE_NAME,A.ORDINAL_POSITION";
+
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlstring, cn);
+
+            System.Data.DataSet d = new System.Data.DataSet();
+
+            da.Fill(d);
+
+            result = JsonConvert.SerializeObject(d, Formatting.Indented);
+
+            d.Dispose();
+            da.Dispose();
+            cn.Close();
+            cn.Dispose();
+
+            return result;
+        }
+
+        [HttpGet]
+        [Route("GetTables")]
+        public string GetTables(string CN)
+        {
+            string result = "RESULT";
+
+            SqlConnection cn = new SqlConnection(CN);
+
+            cn.Open();
+
+            var sqlstring = "SELECT * " +
+                "FROM INFORMATION_SCHEMA.TABLES " +
+                "ORDER BY TABLE_NAME";
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlstring, cn);
+
+            System.Data.DataSet d = new System.Data.DataSet();
+
+            da.Fill(d);
+
+            result = JsonConvert.SerializeObject(d, Formatting.Indented);
+
+            d.Dispose();
+            da.Dispose();
+            cn.Close();
+            cn.Dispose();
+
+            return result;
+        }
+
+        [HttpGet]
+        [Route("GetTableSchema")]
+        public string GetTableSchema(string CN,string TN)
+        {
+            string result = "RESULT";
+
+            SqlConnection cn = new SqlConnection(CN);
+
+            cn.Open();
+
+            var sqlstring = "select A.*," +
+                            "B.TABLE_TYPE," +
+                            "(SELECT OBJECT_ID(A.TABLE_NAME)) as TABLEID," +
+                            "(SELECT IS_IDENTITY FROM SYS.columns SC WHERE SC.object_id = (SELECT OBJECT_ID(A.TABLE_NAME)) AND SC.NAME = A.COLUMN_NAME  ) as IS_IDENTITY " +
+                            "from INFORMATION_SCHEMA.COLUMNS as A " +
+                            "LEFT OUTER JOIN " +
+                            "INFORMATION_SCHEMA.TABLES B on A.TABLE_NAME = B.TABLE_NAME " +
+                            "WHERE A.TABLE_NAME = '" + TN + "' " +
                             "ORDER BY A.TABLE_NAME,A.ORDINAL_POSITION";
 
 
